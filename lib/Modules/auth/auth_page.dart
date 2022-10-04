084cf042a6cee2c67bacaf55b086a041c0684f5c
jaoFranca <joaovfranca1@gmail.com>
2022-10-04 15:39:40 -0300

    fixes

--- lib/Modules/auth/auth_page.dart
+++ lib/Modules/auth/auth_page.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
+<<<<<<< Updated upstream:lib/Modules/auth/auth_page.dart
 class AuthPage extends StatelessWidget {
   const AuthPage({super.key});
+=======
+class HomePage extends StatelessWidget {
+  const HomePage();
+>>>>>>> Stashed changes:lib/Modules/home_page/home_page.dart
 
   @override
   Widget build(BuildContext context) {
